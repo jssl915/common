06bf6674b3b1a766a7d0827b15c00a1435465f36
jssl915 <jssl915@qq.com>
2015-01-23 09:00:15 +0800

    del

--- mysql-mybatis-generator/code/com/system/prg/log/service/SLogService.java
+++
@@ -1,8 +0,0 @@
-package com.system.prg.log.service;
-
-import com.system.prg.log.entity.SLog;
-import com.system.prg.util.BaseService;
-
-public interface SLogService extends BaseService<SLog>{
-
-}
