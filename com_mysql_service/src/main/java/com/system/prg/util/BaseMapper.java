aecb47d564119fb949da535fcbb8c1d930010b13
jssl915 <jssl915@qq.com>
2015-01-29 09:52:05 +0800

    m base

--- com_mysql_service/src/main/java/com/system/prg/util/BaseMapper.java
+++ com_mysql_service/src/main/java/com/system/prg/util/BaseMapper.java
@@ -7,17 +7,17 @@ import org.apache.ibatis.session.RowBounds;
 
 public abstract interface BaseMapper<T>{
 	
-	public abstract void insert(T paramT);
+	public void insert(T paramT);
 	
 	public void deleteByCondition(Map<String, Object> condition);
 	
 	public void updateByCondition(T record);
 	
-	public  List<T> selectByCondition(Map<String,Object> condititon);
+	public List<T> selectByCondition(Map<String,Object> condititon);
 	
 	public Integer countByCondition(Map<String, Object> condititon);
 	
-	public abstract List<T> selectByCondition(Map<String,Object> condititon, RowBounds rowBounds);
+	public List<T> selectByCondition(Map<String,Object> condititon, RowBounds rowBounds);
 	
 	public void deleteByPrimaryKey(Long id);
 
